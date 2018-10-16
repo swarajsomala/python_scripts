1439ffeca0ef4e59a55b2fd071399ab208be7741
swarajsomala <swarajsomala@gmail.com>
2018-10-16 22:54:10 +0530

    Delete pdf_basics.py

--- pdf/pdf_basics.py
+++
@@ -1,6 +0,0 @@
-import PyPDF2
-pdfFileObj = open('AUTOSAR_SWS_RTE.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-pdfReader.numPages
-pageObj = pdfReader.getPage(0)
-pageObj.extractText()
